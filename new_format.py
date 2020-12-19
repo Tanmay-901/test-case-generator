47a452c812730c33867b3e49894a110d3ee6e5fa
Tanmay Khandelwal <58903502+Dude-901@users.noreply.github.com>
2020-12-19 15:13:50 +0530

    Script: send suggested test case format via mail

--- new_format.py
+++ new_format.py
@@ -1,10 +1,10 @@
 import smtplib
 s = smtplib.SMTP('smtp.gmail.com', 587)
 s.starttls()
-s.login("sender_email_ID", "app_password")
+s.login("sender_email_ID", "gmail_App_password")
 message = input("Enter your test case format here(press enter only at the end): ")
 try:
-    s.sendmail("you_email_ID", "receiver_email_ID", message)
+    s.sendmail("sender_email_ID", "receiver_email_ID", message)
     print("Mail sent successfully, Thank you")
     s.quit()
 except SMTPException:
