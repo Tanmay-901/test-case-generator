274f2813e4b695750e82637e2de6a123115c31e7
Tanmay-901 <tanmaykhandelwal2014@gmail.com>
2020-12-30 12:46:51 +0530

    further updates

--- test_case.py
+++ test_case.py
@@ -40,7 +40,7 @@ class Case:
                               , fg='black', command=lambda: Type5(gui), width=11)  # , height=1, width=7)
         self.button6.grid(row=1, column=1, ipady=10, pady=10, padx=5)
         self.button7 = Button(gui, justify=LEFT, text='T\nn  m\nA1  B1\n...   ...\nAm  Bm'
-                              , fg='black', command=lambda: Type5(gui), width=11)  # , height=1, width=7)
+                              , fg='black', command=lambda: Type7(gui), width=11)  # , height=1, width=7)
         self.button7.grid(row=1, column=2, ipady=10, pady=10, padx=5)
         self.button10 = Button(gui, text=' Another type ', fg='black',
                          command=lambda: self.NewFormat(self))  # , height=1, width=7)
@@ -83,8 +83,8 @@ class Case:
         self.button3.grid(row=0, column=2, ipady=10, pady=10, padx=5)
         self.button4.grid(row=0, column=3, ipady=10, pady=10, padx=5)
         self.button5.grid(row=1, column=0, ipady=10, pady=10, padx=5)
-        self.button6.grid(row=2, column=2, ipady=10, pady=10, padx=5)
-        self.button7.grid(row=2, column=2, ipady=10, pady=10, padx=5)
+        self.button6.grid(row=1, column=1, ipady=10, pady=10, padx=5)
+        self.button7.grid(row=1, column=2, ipady=10, pady=10, padx=5)
         self.button10.grid(row=2, column=2, ipady=10, pady=10, padx=5)
 
 class Type1(Case):
@@ -580,6 +580,124 @@ class Type5(Case):
         self.a_max = int(self.maximum_value_of_ai.get())
 
         self.forget_type5()
+        self.display()
+        self.generate()
+
+
+class Type7(Case):
+
+    def __init__(self, master):
+        super(Type7, self).__init__(master)
+        Case.forget_home(self=Case)
+        self.take_input()
+
+    def take_input(self):
+        try:
+            self.output.grid_forget()
+            self.copy_button.grid_forget()
+            self.generate_button.grid_forget()
+            self.change_values_button.grid_forget()
+            self.done_button.grid_forget()
+        except AttributeError:
+            pass
+        self.test_case_count_label = Label(gui, text='T: ', font=('calibre', 10, 'bold'))
+        self.test_case_count = Entry(gui, textvariable=t, font=('calibre', 10, 'normal'))
+        self.minimum_value_of_n = Entry(gui, textvariable=n_min, font=('calibre', 10, 'normal'))
+        self.min_max_values_of_n_label = Label(gui, text='<= n <=', font=('calibre', 10, 'bold'))
+        self.maximum_value_of_n = Entry(gui, textvariable=n_max, font=('calibre', 10, 'normal'))
+        self.minimum_value_of_m = Entry(gui, textvariable=m_min, font=('calibre', 10, 'normal'))
+        self.min_max_values_of_m_label = Label(gui, text='<= m <=', font=('calibre', 10, 'bold'))
+        self.maximum_value_of_m = Entry(gui, textvariable=m_max, font=('calibre', 10, 'normal'))
+        self.minimum_value_of_ai = Entry(gui, textvariable=a_min, font=('calibre', 10, 'normal'))
+        self.min_max_values_of_ai_label = Label(gui, text='<= Ai <=', font=('calibre', 10, 'bold'))
+        self.maximum_value_of_ai = Entry(gui, textvariable=a_max, font=('calibre', 10, 'normal'))
+        self.minimum_value_of_bi = Entry(gui, textvariable=b_min, font=('calibre', 10, 'normal'))
+        self.min_max_values_of_bi_label = Label(gui, text='<= Bi <=', font=('calibre', 10, 'bold'))
+        self.maximum_value_of_bi = Entry(gui, textvariable=b_max, font=('calibre', 10, 'normal'))
+        self.sub_btn = Button(gui, text='Submit', command=self.submit)
+
+        self.test_case_count_label.grid(row=0, column=0)
+        self.test_case_count.grid(row=0, column=1)
+        self.minimum_value_of_n.grid(row=1, column=0)
+        self.min_max_values_of_n_label.grid(row=1, column=1)
+        self.maximum_value_of_n.grid(row=1, column=2)
+        self.minimum_value_of_m.grid(row=2, column=0)
+        self.min_max_values_of_m_label.grid(row=2, column=1)
+        self.maximum_value_of_m.grid(row=2, column=2)
+        self.minimum_value_of_ai.grid(row=3, column=0)
+        self.min_max_values_of_ai_label.grid(row=3, column=1)
+        self.maximum_value_of_ai.grid(row=3, column=2)
+        self.minimum_value_of_bi.grid(row=4, column=0)
+        self.min_max_values_of_bi_label.grid(row=4, column=1)
+        self.maximum_value_of_bi.grid(row=4, column=2)
+        self.sub_btn.grid(row=5, column=1)
+
+    def display(self):
+        self.output = Text(gui, height=5, bg="light cyan")
+        self.output.grid(row=0, column=0, columnspan=10, sticky='n', ipady=10, pady=10, padx=5, )
+        self.copy_button = Button(gui, text='copy', fg='black',
+                                  command=self.cpy)  # , height=1, width=7)
+        self.copy_button.grid(row=1, column=2, sticky='SW', ipady=10, pady=10, padx=5)
+        self.generate_button = Button(gui, text='Re-generate', fg='black',
+                                      command=lambda: self.generate())  # , height=1, width=7)
+        self.generate_button.grid(row=1, column=4, sticky='S', ipady=10, pady=10, padx=5)
+        self.change_values_button = Button(gui, text='Change Constraints', fg='black',
+                                           command=lambda: self.take_input())  # , height=1, width=7)
+        self.change_values_button.grid(row=1, column=5, sticky='S', ipady=10, pady=10, padx=5)
+        self.done_button = Button(gui, text='done', fg='black',
+                                  command=lambda: self.done(self.output))  # , height=1, width=7)
+        self.done_button.grid(row=1, column=7, sticky='SE', ipady=10, pady=10, padx=5)
+
+    def generate(self):
+        # print('generated new')
+        self.output.delete('1.0', END)
+        self.output.insert(END, self.t)
+        self.output.insert(END, '\n')
+
+        for i in range(self.t):
+            self.n = randint(self.n_min, self.n_max)
+            self.m = randint(self.m_min, self.m_max)
+            self.output.insert(END, self.n)
+            self.output.insert(END, ' ')
+            self.output.insert(END, self.m)
+            self.output.insert(END, '\n')
+            for j in range(self.m):
+                self.a = randint(self.a_min, self.a_max)
+                self.b = randint(self.b_min, self.b_max)
+                self.output.insert(END, self.a)
+                self.output.insert(END, ' ')
+                self.output.insert(END, self.b)
+                self.output.insert(END, '\n')
+
+    def forget_type7(self):
+        self.test_case_count_label.grid_forget()
+        self.test_case_count.grid_forget()
+        self.minimum_value_of_n.grid_forget()
+        self.min_max_values_of_n_label.grid_forget()
+        self.maximum_value_of_n.grid_forget()
+        self.minimum_value_of_m.grid_forget()
+        self.min_max_values_of_m_label.grid_forget()
+        self.maximum_value_of_m.grid_forget()
+        self.minimum_value_of_ai.grid_forget()
+        self.min_max_values_of_ai_label.grid_forget()
+        self.maximum_value_of_ai.grid_forget()
+        self.minimum_value_of_bi.grid_forget()
+        self.min_max_values_of_bi_label.grid_forget()
+        self.maximum_value_of_bi.grid_forget()
+        self.sub_btn.grid_forget()
+
+    def submit(self):
+        self.t = int(self.test_case_count.get())
+        self.n_min = int(self.minimum_value_of_n.get())
+        self.n_max = int(self.maximum_value_of_n.get())
+        self.m_min = int(self.minimum_value_of_m.get())
+        self.m_max = int(self.maximum_value_of_m.get())
+        self.a_min = int(self.minimum_value_of_ai.get())
+        self.a_max = int(self.maximum_value_of_ai.get())
+        self.b_min = int(self.minimum_value_of_bi.get())
+        self.b_max = int(self.maximum_value_of_bi.get())
+
+        self.forget_type7()
         self.display()
         self.generate()
 
