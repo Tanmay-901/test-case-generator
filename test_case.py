480685a0ee9056674be57c0ae157bd2edbaef509
Tanmay-901 <tanmaykhandelwal2014@gmail.com>
2021-01-08 19:55:25 +0530

    Added placeholder and Padding on function:take_input screen (up-to type 7)

--- test_case.py
+++ test_case.py
@@ -640,7 +640,7 @@ class Type6(Case):
         self.maximum_value_of_ai.grid(row=2, column=2)
         self.sub_btn.grid(row=3, column=1, pady=(10, 20))
 
-    def display(self):
+    def display(self):                                                      # Type 6
         self.output = Text(gui, height=12, bg="light cyan", width=82)
         self.output.grid(row=0, column=0, columnspan=10, sticky='n', ipady=10, pady=(15, 25), padx=7)
         self.copy_button = Button(gui, text='COPY', fg='black', command=self.cpy)
@@ -653,8 +653,7 @@ class Type6(Case):
         self.done_button = Button(gui, text='DONE', fg='black', command=lambda: self.done(self.output))
         self.done_button.grid(row=1, column=7, sticky='SE', ipady=10, pady=(10, 18), padx=5)
 
-    def generate(self):
-        # print('generated new')
+    def generate(self):                                         # Type 6
         self.output.delete('1.0', END)
         self.n = randint(self.n_min, self.n_max)
         self.m = randint(self.m_min, self.m_max)
@@ -669,7 +668,7 @@ class Type6(Case):
             self.output.insert(END, self.a)
             self.output.insert(END, '\n')
 
-    def forget_type6(self):
+    def forget_type6(self):                                     # Type 6
         self.minimum_value_of_n.grid_forget()
         self.min_max_values_of_n_label.grid_forget()
         self.maximum_value_of_n.grid_forget()
@@ -681,7 +680,7 @@ class Type6(Case):
         self.maximum_value_of_ai.grid_forget()
         self.sub_btn.grid_forget()
 
-    def submit(self):
+    def submit(self):                                          # Type 6
         self.n_min = int(self.minimum_value_of_n.get())
         self.n_max = int(self.maximum_value_of_n.get())
         self.m_min = int(self.minimum_value_of_m.get())
@@ -696,39 +695,42 @@ class Type6(Case):
 
 class Type7(Case):
 
-    def __init__(self, master):
+    def __init__(self, master):                               # Type 7
         super(Type7, self).__init__(master)
         Case.forget_home(self=Case)
         self.take_input()
 
-    def take_input(self):
+    def take_input(self):                                     # Type 7
         try:
             self.output.grid_forget()
             self.copy_button.grid_forget()
             self.generate_button.grid_forget()
             self.change_values_button.grid_forget()
             self.done_button.grid_forget()
+            self.char_list.delete(0, 'end')
         except AttributeError:
             pass
-        self.test_case_count_label = Label(gui, text='T: ', font=('calibre', 10, 'bold'))
+        self.test_case_count_label = Label(gui, text='  T  =   ', font=('calibre', 10, 'bold'))      # Type 7
         self.test_case_count = Entry(gui, textvariable=t, font=('calibre', 10, 'normal'))
+        self.char_list_label = Label(gui, text=' Characters: ', font=('calibre', 10, 'bold'))
+        self.char_list = Entry(gui, textvariable=char_lis, font=('calibre', 10, 'normal'), width=43)
         self.minimum_value_of_n = Entry(gui, textvariable=n_min, font=('calibre', 10, 'normal'))
-        self.min_max_values_of_n_label = Label(gui, text='<= n <=', font=('calibre', 10, 'bold'))
+        self.min_max_values_of_n_label = Label(gui, text='<=Length of string<=', font=('calibre', 10, 'bold'))
         self.maximum_value_of_n = Entry(gui, textvariable=n_max, font=('calibre', 10, 'normal'))
-        self.char_list_label = Label(gui, text='Space separated Characters: ', font=('calibre', 10, 'bold'))
-        self.char_list = Entry(gui, textvariable=char_lis, font=('calibre', 10, 'normal'))
+        self.char_list.insert(END, '(Space separated characters)')
+        self.char_list.bind("<FocusIn>", lambda args: self.char_list.delete('0', 'end'))
         self.sub_btn = Button(gui, text='Submit', command=self.submit)
 
-        self.test_case_count_label.grid(row=0, column=0)
-        self.test_case_count.grid(row=0, column=1)
-        self.minimum_value_of_n.grid(row=1, column=0)
-        self.min_max_values_of_n_label.grid(row=1, column=1)
-        self.maximum_value_of_n.grid(row=1, column=2)
-        self.char_list_label.grid(row=2, column=0)
-        self.char_list.grid(row=2, column=1)
-        self.sub_btn.grid(row=3, column=1)
+        self.test_case_count_label.grid(row=0, column=0, pady=10)               # Type 7
+        self.test_case_count.grid(row=0, column=1, padx=10)
+        self.char_list_label.grid(row=1, column=0, pady=10)
+        self.char_list.grid(row=1, column=1, columnspan=2, padx=10)
+        self.minimum_value_of_n.grid(row=2, column=0, pady=10, padx=10)
+        self.min_max_values_of_n_label.grid(row=2, column=1)
+        self.maximum_value_of_n.grid(row=2, column=2, padx=(10, 10))
+        self.sub_btn.grid(row=3, column=1, pady=(10, 20))
 
-    def display(self):
+    def display(self):                                                      # Type 7
         self.output = Text(gui, height=12, bg="light cyan", width=82)
         self.output.grid(row=0, column=0, columnspan=10, sticky='n', ipady=10, pady=(15, 25), padx=7)
         self.copy_button = Button(gui, text='COPY', fg='black', command=self.cpy)
@@ -741,8 +743,7 @@ class Type7(Case):
         self.done_button = Button(gui, text='DONE', fg='black', command=lambda: self.done(self.output))
         self.done_button.grid(row=1, column=7, sticky='SE', ipady=10, pady=(10, 18), padx=5)
 
-    def generate(self):
-        # print('generated new')
+    def generate(self):                                 # Type 7
         self.output.delete('1.0', END)
         self.output.insert(END, self.t)
         self.output.insert(END, '\n')
@@ -755,7 +756,7 @@ class Type7(Case):
             self.output.insert(END, ''.join(self.a))
             self.output.insert(END, '\n')
 
-    def forget_type7(self):
+    def forget_type7(self):                                 # Type 7
         self.test_case_count_label.grid_forget()
         self.test_case_count.grid_forget()
         self.char_list_label.grid_forget()
@@ -765,7 +766,7 @@ class Type7(Case):
         self.maximum_value_of_n.grid_forget()
         self.sub_btn.grid_forget()
 
-    def submit(self):    # Read Inputs from entry widgets
+    def submit(self):                                       # Type 7
         self.t = int(self.test_case_count.get())
         self.n_min = int(self.minimum_value_of_n.get())
         self.n_max = int(self.maximum_value_of_n.get())
@@ -778,7 +779,7 @@ class Type7(Case):
 
 class Type8(Case):
 
-    def __init__(self, master):
+    def __init__(self, master):                             # Type 8
         super(Type8, self).__init__(master)
         Case.forget_home(self=Case)
         self.take_input()
