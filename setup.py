930e3d3e3163f0ede55cc2038e28c2ce75b87f1e
Tanmay Khandelwal <58903502+Tanmay-901@users.noreply.github.com>
2021-10-06 23:06:48 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-# command: python setup.py py2exe
+# python setup.py install
+# python setup.py py2exe
 from distutils.core import setup
 import py2exe
-setup(windows=['final_release/test_case.py'])
+setup(windows=['final_release/test_case.py']) # relative location of target file
